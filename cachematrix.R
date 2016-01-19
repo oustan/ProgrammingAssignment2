4cbc166dab2f34fded40da067f99c3eaf3a77bd8
oustan <gonzalezjorgeservando@gmail.com>
2016-01-19 15:13:58 +0100

    commiting changes made by OUSTAN

--- cachematrix.R
+++ cachematrix.R
@@ -7,27 +7,31 @@
 ## for cacheSolve function below.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL
-        set<-function(y){
-                x<<-y
-                m<<-NULL
+        m<-NULL ##1 sets the value of m to NULL (provides a default if cacheSolve 
+                ##2 has not yet been used)
+        set<-function(y){ #set the value of the matrix
+                x<<-y   ##1 caches the inputted matrix so that cacheSolve can check 
+                        ##2 whether it has changed (note this is within the set function)
+                m<<-NULL## sets the value of m (the matrix inverse if used cacheSolve) to NULL
         }
-        get<-function()x
-        set_matrix<-function(source) m<<-source
-        get_matrix<-function() m
+        get<-function()x ## gets the value of the non-singular matrix
+        set_matrix<-function(source) m<<-source ##1 calculates the inverse of non-singular 
+                                                ##2 matrix via the solve function
+        get_matrix<-function()                  ## gets the inverse
+                ## passes the value of the function makeCacheMatrix
         list(set=set,get=get,set_matrix=set_matrix,get_matrix=get_matrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {  ## Return a matrix that is the inverse of 'x'
-        m<-x$get_matrix()
-        if(!is.null(m)){
+cacheSolve <- function(x, ...) {  
+        m<-x$get_matrix()       ## used to get the cache of the matrix
+        if(!is.null(m)){        ## if the inverse exists, it gets it.
                 message("getting cached data")
                 return(m)
         }
-        data<-x$get()
+        data<-x$get()   ## if the inverse if not there, first it is calculated and then retrieved.
         m<-source(data, ...)
         x$set_matrix(m)
         m
